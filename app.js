f266ec5577090c74e1a5844efee5fbf8795a4867
Shelby <shelbyhebel@rockconnections.com>
2016-04-05 21:10:50 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+  $('#one').on('mouseover',function(){
+    $('#one').fadeTo(300,.25)
+  });
+  $('#two').on('mouseover',function(){
+    $('#two').fadeTo(300,.25);
+  });
+  $('#three').on('mouseover',function(){
+    $('#three').fadeTo(300,.25);
+  });
+  $('#four').on('mouseover',function(){
+    $('#four').fadeTo(300,.25);
+  });
+
+  $('li').on('click', fuction() {
+    $('this').css
+  )};
+
+
+  $('#one').on('mouseleave',function(){
+    $('#one').fadeTo(300,1)
+  });
+  $('#two').on('mouseleave',function(){
+      $('#two').fadeTo(300,1);
+  });
+  $('#three').on('mouseleave',function(){
+    $('#three').fadeTo(300,1);
+  });
+  $('#four').on('mouseleave',function(){
+    $('#four').fadeTo(300,1);
+  });
+
+
+  $('#one').on('click',function(){
+    $('#one').after("586-790-4497");
+  });
+  $('#two').on('click',function(){
+    $('#two').after("313-338-1248");
+  });
+  $('#three').on('click',function(){
+    $('#three').after("586-913-5305");
+  });
+  $('#four').on('click',function(){
+    $('#four').after("586-777-0853");
+  });
+
+});
